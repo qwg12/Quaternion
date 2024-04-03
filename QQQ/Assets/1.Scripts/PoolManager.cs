b205ff95dfe7de8b0a743ff0a8ac107fc08a47db
qwg12 <a_124@naver.com>
2024-04-03 16:14:54 +0900

    임시

---
+++ QQQ/Assets/1.Scripts/PoolManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+class Pool
+{
+    GameObject _Prefabs;
+    |objectPool|<GameObject> _pool;
+
+    Transform _root;
+    Transform
+}
+
+
+
+public class PoolManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
